4db83fab89bfd45a9cbe08a909782628b70e91ea
MauricioCastro16 <castromauricionicolas@ca.frre.utn.edu.ar>
2024-12-05 14:41:59 -0300

    sacando el Captcha

--- service/backend.js
+++ service/backend.js
@@ -860,30 +860,6 @@ app.post('/api/compitenNuevo', async (req, res) => {
   }
 });
 
-app.post('/api/validate-captcha', async (req, res) => {
-  const { recaptchaResponse } = req.body;
-
-  try {
-    const response = await axios.post(`https://www.google.com/recaptcha/api/siteverify`, null, {
-      params: {
-        secret: process.env.VITE_RECAPTCHA_SECRET_KEY,
-        response: recaptchaResponse
-      }
-    });
-
-    if (response.data.success) {
-      res.json({ success: true });
-    } else {
-      res.json({ success: false });
-    }
-  } catch (error) {
-    res.status(500).json({ success: false, message: 'Error en la verificación' });
-  }
-});
-
-
-
-
 app.post('/api/borrarEvento', async (req, res) => {
   const { nombre_evento, lugar_evento } = req.body;
 
